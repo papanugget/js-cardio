b0e80e6b237bc53b1f3564717816e2e00b7352b4
papanugget <evilgenius04@gmail.com>
2018-11-01 15:17:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// challenge 1 add all nubmers
+// return a sum of all parameters entered regardless of the amount of numbers - no arrays
+// addAll(2,5,6,7) === 20
+
+// solution 1
+function addAll() {
+
+}
+console.log(addAll(2,5,6,7));
+// challenge 2 sum all primes
+// pass in a number to loop 
+
+function sumAllPrimes() {
+
+}
+
+// challenge 3 seek & destroy
+// remove from the array whatever is in the following arguments.  return the leftover numbers in an array
